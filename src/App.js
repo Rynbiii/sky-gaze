b331d83ebfe9d6929ee29f148f4c21040a8c9b72
Loi <hongtailoi231197@gmail.com>
2025-08-20 01:34:24 +0700

    fix app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 // src/App.js
+
+import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/common/NavBar/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/hung/home/Home/Home";
 import Planets from "./components/pages/hung/planets/Planets/Planets";
 import AstronomyTopics from "./components/pages/hung/astronomy_topics/AstronomyTopics/AstronomyTopics";
-import TopicDetail from "./components/pages/hung/astronomy_topics/TopicDetail/TopicDetail"; // 1. Import TopicDetail
+import TopicDetail from "./components/pages/hung/astronomy_topics/TopicDetail/TopicDetail";
 import StarGazing from "./components/pages/loi/star_gazing/StarGazing/StarGazing";
 import Observatories from "./components/pages/loi/observatories/Observatories/Observatories";
 import News from "./components/pages/thai/news/News/News";
@@ -21,16 +23,16 @@ import Comets from "./components/pages/loi/comets/Comets/Comets";
 import { useEffect } from "react";
 import { ScrollToTopButton } from "./components/common/ScrollToTopButton/ScrollToTopButton";
 
-
 function App() {
   return (
     <div className="App">
+      {/* Router bây giờ là HashRouter */}
       <Router>
         <NavBar />
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/sky-gaze" element={<Home />} />
+          {/* Bạn không cần Route "/sky-gaze" nữa vì HashRouter sẽ xử lý */}
           <Route path="/planets" element={<Planets />} />
           <Route path="/astronomy_topics" element={<AstronomyTopics />} />
           <Route path="/astronomy-topics/:topicId" element={<TopicDetail />} />
